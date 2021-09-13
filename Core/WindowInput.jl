6ed55886a225ccfe31cdbd2bf1704a48644ee220
alejandro <alejandrodlsp@hotmail.es>
2021-09-13 22:37:58 +0200

    Update KeyEvent issue

--- Core/WindowInput.jl
+++ Core/WindowInput.jl
@@ -1,31 +1,23 @@
 module WindowInput
 
-import GLFW, ModernGL
+import GLFW
+using ModernGL
 import Event
 
 export RegisterInputCallbacks
 
-# 
-#  Called when a key event is registered (Pressed / Released)
-# 
 function OnKeyCallback(window::GLFW.Window, key::GLFW.Key, scancode::Cint, action::GLFW.Action, mods::Cint)
     if (action == GLFW.PRESS)
         Event.Dispatch(Event.KeyEventData(Event.EventTypeKeyPressed, UInt16(key), 0))
-    else (action == GLFW.RELEASE)
+    elseif (action == GLFW.RELEASE)
         Event.Dispatch(Event.KeyEventData(Event.EventTypeKeyReleased, UInt16(key), 0))
     end
 end
 
-# 
-#  Called when a mouse moved event is registered
-# 
 function OnMouseMovedCallback(window::GLFW.Window, xpos::Cdouble, ypos::Cdouble)
     Event.Dispatch(Event.MouseMovedEventData(Float64(xpos), Float64(ypos)))
 end
 
-# 
-#  Called when a mouse button event is registed (Pressed / Released)
-# 
 function OnMouseButtonCallback(window::GLFW.Window, button::GLFW.MouseButton, action::GLFW.Action, mods::Cint)
     if (action == GLFW.PRESS)
         Event.Dispatch(Event.MouseButtonEventData(Event.EventTypeMouseButtonPressed, UInt16(button)))
@@ -34,38 +26,23 @@ function OnMouseButtonCallback(window::GLFW.Window, button::GLFW.MouseButton, ac
     end
 end
 
-# 
-# 
-# 
 function OnMouseScrollCallback(window::GLFW.Window, offsetX::Cdouble, offsetY::Cdouble)
     Event.Dispatch(Event.MouseScrollEventData(Float64(offsetX), Float64(offsetY)))
 end
 
-# 
-# 
-# 
 function OnWindowCloseCallback(window::GLFW.Window)
     Event.Dispatch(Event.WindowCloseEvent())
 end
 
-# 
-# 
-# 
 function OnWindowSizeCallback(window::GLFW.Window, width::Cint, height::Cint)
     glViewport(0, 0, width, height) # TODO: Move viewport resize call to renderer
     Event.Dispatch(Event.WindowSizeEvent(width, height))
 end
 
-# 
-# 
-# 
 function OnWindowMovedCallback(window::GLFW.Window, posx::Cint, posy::Cint)
     Event.Dispatch(Event.WindowMovedEvent(posx, posy))
 end
 
-# 
-# 
-# 
 function RegisterInputCallbacks(window::GLFW.Window)
     GLFW.SetKeyCallback(window, OnKeyCallback)
     GLFW.SetCursorPosCallback(window, OnMouseMovedCallback)
@@ -75,7 +52,6 @@ function RegisterInputCallbacks(window::GLFW.Window)
     GLFW.SetFramebufferSizeCallback(window, OnWindowSizeCallback)
     GLFW.SetWindowPosCallback(window, OnWindowMovedCallback)
 end
-
 
 # module
 end
