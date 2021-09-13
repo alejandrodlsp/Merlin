1e5b43db5ad5bae4062a1197e99c3894e53e1e95
alejandro <alejandrodlsp@hotmail.es>
2021-09-13 21:31:02 +0200

    Add environment variables

--- Merlin.jl
+++ Merlin.jl
@@ -2,7 +2,13 @@ push!(LOAD_PATH, pwd() * "/Core")
 push!(LOAD_PATH, pwd() * "/Core/Event")
 push!(LOAD_PATH, pwd() * "/Core/Lib")
 
+# Precompile packages
 import Pkg
 Pkg.precompile()
 
+# Load environment variables
+using ConfigEnv
+dotenv()
+
+# Include main application
 include("Core/Application.jl")
