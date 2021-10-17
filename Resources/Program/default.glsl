c57860831ece948057c9b3cafa5df47a84f9308b
alejandro <alejandrodlsp@hotmail.es>
2021-10-17 14:10:02 +0200

    Update default shader

--- Resources/Program/default.glsl
+++ Resources/Program/default.glsl
@@ -8,7 +8,7 @@ out vec4 vertexColor; // specify a color output to the fragment shader
 void main()
 {
     gl_Position = vec4(aPos, 1.0); // see how we directly give a vec3 to vec4's constructor
-    vertexColor = aCol; // set the output variable to a dark-red color
+    vertexColor = vec4(aCol, 1.0); // set the output variable to a dark-red color
 }
 **/VERTEX**
 
